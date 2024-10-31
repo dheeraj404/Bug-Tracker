5cbacf320590c75577804e9152d401d77011ce52
dheeraj404 <130442066+dheeraj404@users.noreply.github.com>
2024-10-31 16:03:11 +0530

    change

--- src/components/ProgressGraph.js
+++ src/components/ProgressGraph.js
@@ -1,7 +1,7 @@
 // src/components/ProgressGraph.js
 import React from 'react';
 import PropTypes from 'prop-types';
-import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
+import { ResponsiveContainer, PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
 
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
@@ -21,23 +21,25 @@ const ProgressGraph = ({ tasks }) => {
   }));
 
   return (
-    <PieChart width={400} height={300}>
-      <Pie
-        data={data}
-        dataKey="value"
-        nameKey="name"
-        cx="50%"
-        cy="50%"
-        outerRadius={80}
-        label
-      >
-        {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-      <Tooltip />
-      <Legend />
-    </PieChart>
+    <ResponsiveContainer width="100%" height={300}>
+      <PieChart>
+        <Pie
+          data={data}
+          dataKey="value"
+          nameKey="name"
+          cx="50%"
+          cy="50%"
+          outerRadius="80%"
+          label
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip />
+        <Legend />
+      </PieChart>
+    </ResponsiveContainer>
   );
 };
 
